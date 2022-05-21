2e8bb1a291fb59c4f5bc40fdcddf288602b53bd4
Юрий Данильченко <genitra111@gmail.com>
2022-05-21 14:29:30 +1000

    Добавлен код без Stream API

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,18 @@
 package src;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] list = new int[]{1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
-
-        List<Integer> intList = new ArrayList<>();
-
-
-        for (int i = 0; i < list.length; i++) {
-            intList.add(list[i]);
-        }
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
         Collections.sort(intList);
-        for (int i = 0; i < intList.size(); i++) {
-            if (intList.get(i) > 0) {
-                if (intList.get(i) %2 == 0) {
-
-                    // если сортировку сделать в этом месте, то в список попадает число -1
-                    // не могу понять почему
-                    //Collections.sort(intList);
-                    System.out.print(intList.get(i) + ", ");
-                }
+        for (int in : intList) {
+            if (in > 0 && in % 2 == 0) {
+                    System.out.print(in + " ");
             }
         }
     }
